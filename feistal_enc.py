4dbcf9e6ff800170044dead65ebb9d43ec477c48
Kush Dilip Acharya <31572699+kush-1277@users.noreply.github.com>
2020-04-25 11:11:18 -0400

    Update feistal_enc.py
    Can be called from a separate python code by using the following code
    
    from homework3 import feistel_enc_test 
        print("\n============================ FEISTAL_ENC ======================================\n")
        feistel_enc_test("test.txt", 5, 16, "test_enc.txt")
        finp = open("test_enc.txt", 'rb')
        inpbyteseq = finp.read()
        finp.close()
        print(inpbyteseq)
    if __name__=="__main__":
        test_module()

--- feistal_enc.py
+++ feistal_enc.py
@@ -60,6 +60,10 @@ def feistel_enc(inputblock, num_rounds, seed):
 
 #################################### TEST MAIN ENCRYPTION #####################################
 def feistel_enc_test(input_fname, seed, num_rounds, output_fname):
+    # input_fname is the filename that we want to encrypt.
+    # seed is the starting point from hwere our random keys will be generated
+    # num_rounds = number of rounds = 16
+    # output_fname is the name of the outputfile
     finp = open(input_fname, 'rb')
     inpbyteseq = finp.read()
     finp.close()
